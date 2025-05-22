2516b1d02aa387c6bbd800274861a7c17826be9a
Julio Apaza <79545504+JulioApazaZhukov@users.noreply.github.com>
2025-05-21 21:06:26 -0500

    05-21-2025

--- PythonforFinance-3.py
+++ PythonforFinance-3.py
@@ -8,6 +8,7 @@ df = pd.read_csv('^GSPC_L.csv', parse_dates=True, index_col=0)
 
 #   df['100ma'] = df['Adj Close'].rolling(window=100).mean()
 df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+df['200ma'] = df['Adj Close'].rolling(window=200, min_periods=0).mean()
 
 #   df.dropna(inplace=True)
 
@@ -18,6 +19,7 @@ ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
 
 ax1.plot(df.index, df['Adj Close'])
 ax1.plot(df.index, df['100ma'])
+ax1.plot(df.index, df['200ma'])
 ax2.bar(df.index, df['Volume'])
 
 plt.show()
